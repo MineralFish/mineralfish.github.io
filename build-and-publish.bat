87db6b2e4db9d51199c05d36a3e2ed754d4f7d33
CST1229 <68464103+CST1229@users.noreply.github.com>
2022-01-24 20:27:35 +0100

    Update build script

--- build-and-publish.bat
+++ build-and-publish.bat
@@ -4,22 +4,29 @@ rem Build this site and copy it to the build branch. Also push it. Requires git 
 
 cls
 
-del "..\temp-built-site"
+del /q "..\temp-built-site"
 
+echo Switching branch
 git checkout master
+echo Installing dependencies
 npm install
+echo Building
 npm run build
 
 md "..\temp-built-site"
 
-xcopy /s ".\public" "..\temp-built-site\"
+echo Copying to temp folder
+xcopy /s ".\public" "..\temp-built-site\" > nul
 
+echo Switching branch
 git checkout build
 
+echo Copying from temp folder
 xcopy /s "..\temp-built-site" ".\"
 
-del "../temp-built-site"
+del /q "../temp-built-site"
 
+echo Switching branch
 git checkout master
 
 echo Done. Switch to the build branch, commit and push.
